f233c2c6fef5b8c712292120e3126b3d910b9455
VadimCat <gergon1999@gmail.com>
2019-10-05 20:56:50 +0300

    fuck it

---
+++ Assets/LevelCreator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[System.Serializable]
+public struct Assotiation
+{
+    public Color color;
+    public GameObject prefab;
+}
+
+[System.Serializable]
+public struct Layer
+{
+    const int TERRAIN_SCALE = 10;
+    const int SEMANTICS_SCALE = 1;
+    public List<Assotiation> Terrain;
+    public List<Assotiation> Semantics;
+
+    public Texture2D texture;
+
+    public void GenerateLevel()
+    {
+        //TERRAIN
+        Vector3 offset = new Vector3(-(float)texture.width / 2, 0, -(float)texture.height / 2);
+        for (int i = 0; i < texture.width; i++)
+        {
+            for (int j = 0; j < texture.height; j++)
+            {
+                Color color = texture.GetPixel(i, j);
+                int index = Terrain.FindIndex((assotiation) => assotiation.color == color);
+                Debug.Log(i + " "+ j);
+                Debug.Log(index);
+                Debug.Log((Color32)color);
+                MonoBehaviour.Instantiate(Terrain[index].prefab,
+                    new Vector3(i * TERRAIN_SCALE, 0, j * TERRAIN_SCALE),
+                    Quaternion.identity);
+            }
+        }
+    }
+
+}
+public class LevelCreator : MonoBehaviour
+{
+    public static LevelCreator Instance;
+
+    public Layer[] LevelLayers;
+
+    private void Awake()
+    {
+        LevelLayers[0].GenerateLevel();
+    }
+
+}
