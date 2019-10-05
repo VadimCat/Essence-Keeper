e9a749424f08cd5f817b8e58dbd8a2270f80e516
Shmabulock <dima.kahnovich@gmail.com>
2019-10-05 17:14:28 +0300

    charger basic lifecycle done

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-    /*private void OnEnable()
-    {
-        enemyCount++;
-    }*/
+public abstract class Enemy : MonoBehaviour
+{
 
-    void GiveBonus()
+    [SerializeField] int _health;
+    public int Health
     {
-
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+        }
+    }
+    [SerializeField] int _attackPower;
+    public int AttackPower
+    {
+        get
+        {
+            return _attackPower;
+        }
+        set
+        {
+            _attackPower = value;
+        }
+    }
+    [SerializeField] int _moveSpeed;
+    public int MoveSpeed
+    {
+        get
+        {
+            return _moveSpeed;
+        }
+        set
+        {
+            _moveSpeed = value;
+        }
     }
 
-    public void EnemyRemove()
-    {
+    protected Rigidbody _rBody;
+   
 
+    protected abstract void Move(Vector3 point);
+    protected abstract IEnumerator Attack(GameObject target);
+    protected abstract IEnumerator Wait(float seconds);
+    protected void Die()
+    {
+        EventManager.Instance.OnEnemyDie();
+        Destroy(gameObject);
     }
+
 }
+
+
